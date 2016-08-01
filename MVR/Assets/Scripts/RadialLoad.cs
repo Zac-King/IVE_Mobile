0e3c103c943cb1f0e5f03360f3f5186dc037bf22
Zac King <ZacKingx@gmail.com>
2016-08-01 14:25:05 -0500

    Radial loader

---
+++ MVR/Assets/Scripts/RadialLoad.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Image))]
+public class RadialLoad : MonoBehaviour
+{
+    #region Variables
+    private Image m_progressCircle;                     // 
+    [SerializeField] private float m_loadTime = 3f;     // Seconds to complete load
+    [SerializeField] private Gradient m_loadGradient;   // Gradient over time for load
+    public bool m_loading = false;                      // Bool for current state of loading 
+   
+    #endregion
+    // MonoBehaviour ///////////////////////////////////////////////////////////////////////////////////
+    void Awake()    // If not already set up, it will be fixed on awake
+    {
+        m_progressCircle = gameObject.GetComponent<Image>();
+        m_progressCircle.type = Image.Type.Filled;
+    }
+    
+    // Context Menu Functions //////////////////////////////////////////////////////////////////////////
+    [ContextMenu("Set the Type to Fill")]   // Set image type without having to play scene
+    private void Setup()
+    {
+        m_progressCircle = gameObject.GetComponent<Image>();
+        m_progressCircle.type = Image.Type.Filled;
+    }
+
+    [ContextMenu("Test Load")]
+    private void testLoad()                 // Simulate an outside function influence on script
+    {
+        Callback c = assdf;
+        m_loading = true;
+        LoadTarget(3, c);
+    }
+
+    void assdf()
+    {
+        Debug.Log("Loading complete");
+    }
+    
+    // Functions ///////////////////////////////////////////////////////////////////////////////////////
+    public void LoadTarget(float loadTime, Callback a)
+    {
+        m_loadTime = loadTime;
+        StartCoroutine(LoadCircle(a));
+    }
+
+    IEnumerator LoadCircle(Callback callback)    // Load coroutine
+    {
+        float timer = 0;
+        while (timer <= m_loadTime && m_loading)
+        {
+            m_progressCircle.fillAmount = timer / m_loadTime;
+            m_progressCircle.color = m_loadGradient.Evaluate(timer / m_loadTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        if(m_loading)
+        {
+            callback();
+        }
+
+        m_progressCircle.fillAmount = 0;   // Reset
+    }
+}
---
+++ MVR/Assets/Scripts/Silhouette.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Silhouette : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve pulseRate;
+
+	[ContextMenu("Pulse")]
+	void StartItUp ()
+    {
+        StartCoroutine(HighLightPulse(1));
+	}
+
+    IEnumerator HighLightPulse(float time)
+    {
+        while (true)
+        {
+            float timer = 0;
+            while (timer <= time)
+            {
+                transform.localScale = new Vector3(pulseRate.Evaluate(timer / time), pulseRate.Evaluate(timer / time), pulseRate.Evaluate(timer / time));
+                timer += Time.deltaTime;
+                yield return null;
+            }
+            yield return null;
+        }
+    }
+}
